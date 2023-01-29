bd1a07c1ddf9807a3bbbdc5dd15871bfceb58f73
zzihyeon <guswldb123@gmail.com>
2023-01-29 19:10:47 +0900

    feat: add mongoDB modules & new.json output file

--- server.js
+++ server.js
@@ -1,71 +1,68 @@
-const axios = require("axios");
-const cheerio = require("cheerio");
-const http = require('http');
+const axios = require('axios');
+const cheerio = require('cheerio');
 const fs = require('fs');
-var iconv  = require('iconv-lite');
+var iconv = require('iconv-lite');
+const MongoClient = require('mongodb').MongoClient;
 
-async function main(maxPages = 50) {
-    // initialized with the first webpage to visit
-    const paginationURLsToVisit = [];
-    for (let i=1;i<10;i++) {
-        for (let j=1;j<3;j++) {
-            paginationURLsToVisit.push(`http://www.playdb.co.kr/playdb/playdblist.asp?Page=${j}&sReqMainCategory=00000${i}&sReqSubCategory=&sReqDistrict=&sReqTab=2&sPlayType=3&sStartYear=&sSelectType=1`)
+async function main() {
+    const conn = await MongoClient.connect('mongodb://localhost:27017');
+    const db = conn.db('event')
+    const new_event_collection = db.collection('new_event');
+    const all_data = await new_event_collection.find({}).toArray(); //현재 확인한 모든 event
+    const prev_key = {}; // 이미 존재하는 key
+    all_data.map((ele) => {
+        prev_key[ele.key] = true;
+    })
+    const urls = [];
+    const keyMap = {}; //현재 category에서 존재하는 key
+    for (let i = 1; i < 10; i++) {
+        for (let j = 1; j < 10000; j++) {
+            const paginationURL = `http://www.playdb.co.kr/playdb/playdblist.asp?Page=${j}&sReqMainCategory=00000${i}&sReqSubCategory=&sReqDistrict=&sReqTab=2&sPlayType=3&sStartYear=&sSelectType=1`;
+            console.log(paginationURL)
+            const pageHTML = await axios.get(paginationURL, { responseType: 'arraybuffer' });
+
+            const detailMap = {};
+            const content = iconv.decode(pageHTML.data, 'EUC-KR').toString()
+            const $ = cheerio.load(content);
+
+            // console.log(pageHTML.data);
+            $('a').each((index, element) => {
+                const tpaginationURL = $(element).attr('href');
+                if (tpaginationURL.includes('#')) {
+                    const detail = $(element).attr('onclick');
+                    const detailKey = detail.match(/[0-9]+/)[0];
+                    const context = $(element).text();
+                    detailMap[detailKey] = context;
+                }
+            })
+            let stopFlag = false;
+            const newURLs = Object.keys(detailMap).map((key) => {
+                if (prev_key[key]) {
+                    return null;
+                }
+                if (!keyMap[key]) {
+                    keyMap[key] = true;
+                } else {
+                    return null;
+                }
+                return { name: detailMap[key], key: key, url: `http://www.playdb.co.kr/playdb/playdbDetail.asp?sReqPlayno=${key}`, created_at: new Date() }
+            }).filter((ele) => ele !== null)
+            if (newURLs.length === 0) {
+                break;
+            }
+            urls.push(...newURLs)
         }
     }
-    const visitedURLs = [];
-    // iterating until the queue is empty
-    // or the iteration limit is hit
-    const urls = [];
-    while (
-        paginationURLsToVisit.length !== 0 &&
-        visitedURLs.length <= maxPages
-        ) {
-        // the current webpage to crawl
-        const paginationURL = paginationURLsToVisit.pop();
-
-        // retrieving the HTML content from paginationURL 
-        const pageHTML = await axios.get(paginationURL,{
-            Headers:{'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8'}
-        });
-        
-        // const strContents_prev = iconv.decode(pageHTML.data,'euckr')
-        // const strContents = iconv.encode(strContents_prev,'utf8')
-        // fs.writeFileSync('zz.txt',strContents)
-        const detailMap = {};
-        // adding the current webpage to the
-        // web pages already crawled
-        visitedURLs.push(paginationURL);
-
-
-        // initializing cheerio on the current webpage
-        const $ = cheerio.load(pageHTML.data);
-
-        // console.log(pageHTML.data);
-        $('a').each((index, element) => {
-            const tpaginationURL = $(element).attr("href");
-            if (tpaginationURL.includes("#")) {
-                const detail = $(element).attr("onclick");
-                const detailKey = detail.slice(10,16);
-                const context = $(element).text();
-                detailMap[detailKey] = context;
-            }
-        })
-        
-        const newURLs = Object.keys(detailMap).map((key)=>{
-            return {name: detailMap[key],key: key, url: `http://www.playdb.co.kr/playdb/playdbDetail.asp?sReqPlayno=${key}`}
-        })
-        urls.push(...newURLs)
+    if (urls.length !== 0) {
+        await new_event_collection.insertMany(urls)
     }
-    fs.writeFileSync('new.json',JSON.stringify(urls))
+    fs.writeFileSync(`new_${new Date().getTime()}.json`, JSON.stringify(urls))
 }
-
 main()
     .then(() => {
         process.exit(0);
     })
     .catch((e) => {
-        // logging the error message
         console.error(e);
-
         process.exit(1);
     });
